dc7241a432705f8ad49267e2435b1f4e5d08b625
lostsquirrel <songli1988@gmail.com>
2017-04-17 17:14:36 +0800

    add link in chart to data manage

--- static/item.js
+++ static/item.js
@@ -49,29 +49,3 @@ function saveItem(name) {
          loadItemList();
      });
  }
-function myGet(url, callback) {
-    $.get(url, callback);
-}
-
-function myPost(url, data, callback) {
-    myAjax(url, 'post',  data, callback);
-}
-
-function myPut(url, data, callback) {
- myAjax(url, 'put',  data, callback);
-}
-
-function myDelete(url, data, callback) {
-    myAjax(url, 'delete',  data, callback);
-}
-
-function myAjax(url, method,  data, callback) {
-    $.ajax({
-        url: url,
-        data: JSON.stringify(data),
-        success: callback,
-        dataType: 'json',
-        method: method,
-        contentType: 'application/json; charset=UTF-8'
-    });
-}
