95e37ba751e0524e4d447fec72a668726566f469
lostsquirrel <songli1988@gmail.com>
2017-04-20 14:10:17 +0800

    添加交易评估数据管理功能

--- settings.py
+++ settings.py
@@ -6,14 +6,13 @@ Created on 2013-3-26
 '''
 import os
 
-installed_apps = ['futures', 'api']
+installed_apps = ['futures', 'api', 'evaluation']
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 static_dir = os.path.join(os.path.dirname(__file__), "static")
 db_user = 'futures'
 db_password = 'futures_pass'
 db_name = 'futures'
-db_host = '192.168.1.139:13301'
-# db_host = 'mariadb_10_1_main'
+db_host = 'mariadb_10_1_main'
 pass_code = 'a'
 port = 9000
---
+++ static/evaluation_data.js
@@ -0,0 +1,43 @@
+$(function () {
+    bindRange('evaluationScore');
+    bindSubmit();
+    loadDataList();
+});
+
+var evaluationDataUrl = '/api/evaluation/data'
+function bindSubmit() {
+    $('#dataForm').submit(function(){
+        return false;
+    })
+    $('input[type="submit"]').click(function(){
+        myPost(evaluationDataUrl, objectifyForm('dataForm'), function(data){
+            // console.log(data)
+            loadDataList()
+        });
+    });
+}
+
+function removeData(id) {
+    myDelete(evaluationDataUrl, {id: id}, function(){
+        loadDataList()
+    })
+}
+
+function loadDataList() {
+    myGet(evaluationDataUrl, function(data){
+        $('#dataList').find('.dataNode').remove();
+        var h = '';
+        $(data).each(function(k, v) {
+            h += '<tr class="dataNode">';
+            h += '<td>'+v.trade_date+'</td>';
+            h += '<td>'+v.position_time+'秒</td>';
+            var profitClass = v.profit > 0 ? 'win' : 'lose'
+            h += '<td><span class="'+(profitClass)+'">'+v.profit+'</span></td>';
+            h += '<td>'+v.commission+'</td>';
+            h += '<td>'+v.evaluation_score+'</td>';
+            h += '<td><a href="javascript:removeData('+v.id+')" >删除</a></td>';
+            h += '</tr>';
+        });
+        $('#dataList').append(h);
+    });
+}
