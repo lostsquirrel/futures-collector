860b2934aba443deffc5c937a5c8173bbf5a74db
ArcDemon <arc.demon@qq.com>
2018-11-02 22:56:30 +0800

    增加了手机端页面的mapping
    -

--- evaluation/views.py
+++ evaluation/views.py
@@ -2,12 +2,32 @@
 
 from simpletor import application
 
+
 @application.RequestMapping("/evaluation/data")
 class EvaluationDataHandler(application.RequestHandler):
     def get(self):
         self.render('evaluation_data.html')
 
+
 @application.RequestMapping("/evaluation/stats")
 class EvaluationStatsHandler(application.RequestHandler):
     def get(self):
         self.render('evaluation_stats.html')
+
+
+@application.RequestMapping("/evaluation/entry")
+class EvaluationDataHandler(application.RequestHandler):
+    def get(self):
+        self.render('wap_evaluation_entry.html')
+
+
+@application.RequestMapping("/evaluation/history")
+class EvaluationStatsHandler(application.RequestHandler):
+    def get(self):
+        self.render('wap_evaluation_history.html')
+
+
+@application.RequestMapping("/evaluation/total")
+class EvaluationStatsHandler(application.RequestHandler):
+    def get(self):
+        self.render('wap_total.html')
